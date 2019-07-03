42dd5bafc7d0cd8170e36e80631500434c659fe7
Cameron <camearle20@me.com>
2019-07-03 16:44:24 -0400

    Basic serial handler for Xbee frames

---
+++ include/Constants.h
@@ -0,0 +1,16 @@
+//
+// Created by cameronearle on 7/3/2019.
+//
+
+#ifndef SAFETYCONTROLLER_CONSTANTS_H
+#define SAFETYCONTROLLER_CONSTANTS_H
+
+#define ESTOP 0
+#define ENABLE 1
+
+#define XBEE_SERIAL_BAUDRATE 9600 //The baudrate to use when communicating with the Xbee
+#define XBEE_SERIAL_BUFFER_SIZE 100
+
+#define RELAY_PIN 6
+
+#endif //SAFETYCONTROLLER_CONSTANTS_H
